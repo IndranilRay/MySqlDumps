abd2fae44f3214f14268482dcf01f7d123cc84f9
neal <thecodejarvis@gmail.com>
2018-08-08 22:46:10 +0530

    Add db configuration

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+/***** EDIT BELOW LINES *****/
+$DB_Server = "localhost"; // MySQL Server
+$DB_Username = "root"; // MySQL Username
+$DB_Password = "root"; // MySQL Password
+$DB_DBName = "metrics_air_quality"; // MySQL Database Name
+$DB_TBLName = "madrid_2001"; // MySQL Table Name
+$mysqli = new mysqli('localhost', 'root', 'root', 'metrics_air_quality');
